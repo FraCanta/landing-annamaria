792fa15e0f40220ec23538f71d8a6d2905a03222
FraCanta <you@example.comfcantale14@gmail.com>
2024-08-09 16:54:08 +0200

    landing

--- pages/index.js
+++ pages/index.js
@@ -38,7 +38,7 @@ export default function Home() {
         <Menu />
         <main className="flex flex-col">
           <div className="grid grid-cols-1 lg:grid-cols-2">
-            <div className="flex flex-col gap-6 pb-10 border-r lg:pr-4">
+            <div className="flex flex-col gap-6 pb-10 pr-3 border-r lg:pr-4">
               <h1 className="text-4xl 2xl:text-[3rem] 2xl:leading-[3.1rem] 2xla:text-6xl text-main mt-10">
                 La passione mi ha portato qui. Sono convinta che un lavoro non
                 sia solo un lavoro. <br />
@@ -53,7 +53,7 @@ export default function Home() {
                 <Icon icon="system-uicons:download" className="w-6 h-6" />
               </CtaPurple>
             </div>
-            <div className="relative border-l lg:border-l-0 border-r border-b border-t aspect-square lg:w-full lg:h-[60vh] 2xla:h-[60vh] border-main">
+            <div className="relative border-l lg:border-l-0 border-r border-b border-t lg:border-t-0 aspect-square lg:w-full lg:h-[60vh] 2xla:h-[60vh] border-main">
               <Image
                 src={"/assets/hero_img2.webp"}
                 fill
@@ -87,7 +87,7 @@ export default function Home() {
                 </div>
               </div>
             </div>
-            <div className="relative flex flex-col w-full p-4">
+            <div className="relative flex flex-col w-full p-1 lg:p-4">
               <div className="relative flex justify-start w-full">
                 <Icon
                   icon="bxs:quote-alt-left"
