bd5538d8f9a15e784ddd226147ed862ce7b99a2e
FraCanta <you@example.comfcantale14@gmail.com>
2024-08-12 17:49:37 +0200

    sitemap e robots

--- pages/_app.js
+++ pages/_app.js
@@ -9,22 +9,24 @@ export default function App({ Component, pageProps }) {
         async
         src="https://www.googletagmanager.com/gtag/js?id=G-7XWPH0Q69T"
       ></Script>
-      <Script id="google-analytics" strategy="afterInteractive" defer>
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-         gtag('js', new Date());
-
-
-      gtag('config', 'G-D8CRBEXC6K'), { 'debug_mode':true });
-        `}
-      </Script>
       <Script id="google-tag-manager" strategy="afterInteractive">
         {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-KMP7PQWF');`}
+      </Script>
+      {/* <!-- Google tag (gtag.js) --> */}
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-D8CRBEXC6K"
+      ></Script>
+      <Script id="google-analytics" strategy="afterInteractive" defer>
+        {` window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-D8CRBEXC6K');`}
       </Script>
     </>
   );
