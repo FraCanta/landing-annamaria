4a6ed8bcb06ad1d611852afe998aea6f9ad7550f
FraCanta <you@example.comfcantale14@gmail.com>
2024-08-12 19:56:11 +0200

    tag menager

--- pages/index.js
+++ pages/index.js
@@ -30,14 +30,14 @@ export default function Home({ translation, locale }) {
           />
           <Script
             async
-            src="https://www.googletagmanager.com/gtag/js?id=G-7XWPH0Q69T"
+            src="https://www.googletagmanager.com/gtag/js?id=G-D8CRBEXC6K"
           ></Script>
           <Script id="google-tag-manager" strategy="afterInteractive">
             {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-KMP7PQWF');`}
+})(window,document,'script','dataLayer','GTM-WT672BZX');`}
           </Script>
           {/* <!-- Google tag (gtag.js) --> */}
           <Script id="google-analytics" strategy="afterInteractive" defer>
