b2a8c6724662eb2406b70b25d8df9ea9811bc8f3
FraCanta <you@example.comfcantale14@gmail.com>
2024-08-12 17:38:35 +0200

    sitemap e robots

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,37 +1,17 @@
 // pages/sitemap.xml.js
 const headlessSite = "https://annamariaricci.eu";
 
-function generateSiteMap() {
-  return `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-      <url>
-        <loc>${headlessSite}/</loc>
-        <changefreq>weekly</changefreq>
-        <priority>1.0</priority>
-      </url>
-      <url>
-        <loc>${headlessSite}/en</loc>
-        <changefreq>weekly</changefreq>
-        <priority>1.0</priority>
-      </url>
-    </urlset>
-  `;
-}
-
-function SiteMap() {
-  // getServerSideProps will do the heavy lifting
-}
-
-export async function getServerSideProps({ res }) {
-  const sitemap = generateSiteMap();
-
-  res.setHeader("Content-Type", "text/xml");
-  res.write(sitemap);
-  res.end();
-
-  return {
-    props: {},
-  };
-}
-
-export default SiteMap;
+<xml version="1.0" encoding="UTF-8">
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+      <loc>${headlessSite}/</loc>
+      <changefreq>weekly</changefreq>
+      <priority>1.0</priority>
+    </url>
+    <url>
+      <loc>${headlessSite}/en</loc>
+      <changefreq>weekly</changefreq>
+      <priority>1.0</priority>
+    </url>
+  </urlset>
+</xml>;
