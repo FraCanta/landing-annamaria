893fb2be4f5cc79f3539843463464897a68a7329
FraCanta <you@example.comfcantale14@gmail.com>
2024-08-12 18:03:06 +0200

    sitemap e robots

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import CtaPurple from "@/components/layout/Cta/CtaPurple";
 import { Icon } from "@iconify/react";
 import homeIT from "@/public/locales/it/home.json";
 import homeEN from "@/public/locales/en/home.json";
+import Script from "next/script";
 
 export default function Home({ translation, locale }) {
   console.log(translation);
@@ -21,6 +22,25 @@ export default function Home({ translation, locale }) {
             name="google-site-verification"
             content="nrHV2MQ_ziMHOzgrljTK-sECIJk0ydVLb1GPPYIpQ_w"
           />
+          <Script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-7XWPH0Q69T"
+          ></Script>
+          <Script id="google-tag-manager" strategy="afterInteractive">
+            {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KMP7PQWF');`}
+          </Script>
+          {/* <!-- Google tag (gtag.js) --> */}
+          <Script id="google-analytics" strategy="afterInteractive" defer>
+            {` window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-D8CRBEXC6K', { 'debug_mode':true });`}
+          </Script>
           <link
             rel="canonical"
             href={`https://www.annamariaricci.eu/${locale.locale}`}
