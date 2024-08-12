02879500a587067543028f7c12a19ed120bba44f
FraCanta <you@example.comfcantale14@gmail.com>
2024-08-12 20:03:54 +0200

    tag menager

--- pages/index.js
+++ pages/index.js
@@ -28,24 +28,17 @@ export default function Home({ translation, locale }) {
             title="Sitemap"
             href="../sitemap.xml"
           />
+          {/* <!-- Google tag (gtag.js) --> */}
           <Script
             async
-            src="https://www.googletagmanager.com/gtag/js?id=G-D8CRBEXC6K"
+            src="https://www.googletagmanager.com/gtag/js?id=G-BL0G9HLEHG"
           ></Script>
-          <Script id="google-tag-manager" strategy="afterInteractive">
-            {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-WT672BZX');`}
-          </Script>
-          {/* <!-- Google tag (gtag.js) --> */}
           <Script id="google-analytics" strategy="afterInteractive" defer>
-            {` window.dataLayer = window.dataLayer || [];
+            {`window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'G-D8CRBEXC6K', { 'debug_mode':true });`}
+  gtag('config', 'G-BL0G9HLEHG', { 'debug_mode':true });`}
           </Script>
           <link
             rel="canonical"
